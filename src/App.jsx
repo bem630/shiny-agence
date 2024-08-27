09ebeb45dfa8e08157e46b69d643d36cc412e6eb
Elhadj Mamadou Bah <bah70495@gmail.com>
2024-08-27 02:37:53 +0100

    ✨  P1C2-begin: Delplacement de <Survey/>

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Home from './components/pages/Home/Home'
-import Survey from './components/pages/Survey'
+import Survey from './components/pages/Survey/Survey'
 import Error from './components/Error/Error'
+import Results from './components/pages/Results/Results'
+import Freelances from './components/pages/Freelances/Freelances'
 
 function App() {
 
@@ -12,7 +14,9 @@ function App() {
       <Header/>
       <Routes>
         <Route path='/' element={<Home/>} />
-        <Route path='/survey' element={<Survey/>} />
+        <Route path='/survey/:questionNumber' element={<Survey/>} />
+        <Route path='/results' element={<Results/>} />
+        <Route path='/freelances' element={<Freelances/>} />
         <Route path='*' element={<Error/>} />
       </Routes>
     </>
