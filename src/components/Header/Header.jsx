06de7cb264e6728b26f2f6c45e4f0f8672eaefdb
Elhadj Mamadou Bah <bah70495@gmail.com>
2024-08-27 01:45:04 +0100

    📦 P1C2-begin: HEADER > Ajout des liens vers les autres pages

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <nav>
+        <Link to="/">Accueil</Link>
+        <Link to="/survey">Questionnaire</Link>
+    </nav>
   )
 }
