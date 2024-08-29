ded8b6f453104170b20345c501270a005cef3847
Elhadj Mamadou Bah <bah70495@gmail.com>
2024-08-29 06:07:30 +0100

    ✨ P1C4-begin: Footer > Ajout de Resultat

--- src/components/pages/Survey/Survey.jsx
+++ src/components/pages/Survey/Survey.jsx
@@ -1,19 +1,121 @@
-import React from 'react'
-import { Link, useParams } from 'react-router-dom'
+import { useContext } from 'react'
+import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import colors from '../../../utils/style/colors'
+import { Loader } from '../../../utils/style/Atoms' 
+import { SurveyContext } from '../../../utils/context'
+import { useFetch, useTheme } from '../../../utils/hooks'
 
-export default function Survey() {
+const SurveyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const QuestionTitle = styled.h2`
+  text-decoration: underline;
+  text-decoration-color: ${colors.primary};
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
+`
+
+const QuestionContent = styled.span`
+  margin: 30px;
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
+`
+
+const LinkWrapper = styled.div`
+  padding-top: 30px;
+  & a {
+    color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
+  }
+  & a:first-of-type {
+    margin-right: 20px;
+  }
+`
+
+const ReplyBox = styled.button`
+  border: none;
+  height: 100px;
+  width: 300px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) =>
+    theme === 'light' ? colors.backgroundLight : colors.backgroundDark};
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
+  border-radius: 30px;
+  cursor: pointer;
+  box-shadow: ${(props) =>
+    props.$isSelected ? `0px 0px 0px 2px ${colors.primary} inset` : 'none'};
+  &:first-child {
+    margin-right: 15px;
+  }
+  &:last-of-type {
+    margin-left: 15px;
+  }
+`
+
+const ReplyWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+function Survey() {
   const { questionNumber } = useParams()
   const questionNumberInt = parseInt(questionNumber)
-  const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumber - 1
-  const nextQuestionNumber = questionNumber + 1
+  const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1
+  const nextQuestionNumber = questionNumberInt + 1
+  const { theme } = useTheme()
+
+  const { saveAnswers, answers } = useContext(SurveyContext)
+
+  function saveReply(answer) {
+    saveAnswers({ [questionNumber]: answer })
+  }
+  const { data, isLoading, error } = useFetch(`http://localhost:8000/survey`)
+  const surveyData = data?.surveyData
+
+  if (error) {
+    return <span>Oups il y a eu un problème</span>
+  }
+
   return (
-    <div>
-      <h1>Questionnaire 🧮</h1>
-      <h2>Question {questionNumber}</h2>
-      <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
-      {questionNumber === 10 ? ( <Link to="/results">Resultats</Link>) : (
-        <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
-      ) }
-    </div>
+    <SurveyContainer>
+      <QuestionTitle theme={theme}>Question {questionNumber}</QuestionTitle>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <QuestionContent theme={theme}>
+          {surveyData[questionNumber]}
+        </QuestionContent>
+      )}
+      <ReplyWrapper>
+        <ReplyBox
+          onClick={() => saveReply(true)}
+          $isSelected={answers[questionNumber] === true}
+          theme={theme}
+        >
+          Oui
+        </ReplyBox>
+        <ReplyBox
+          onClick={() => saveReply(false)}
+          $isSelected={answers[questionNumber] === false}
+          theme={theme}
+        >
+          Non
+        </ReplyBox>
+      </ReplyWrapper>
+      <LinkWrapper theme={theme}>
+        <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
+        {surveyData && surveyData[questionNumberInt + 1] ? (
+          <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
+        ) : (
+          <Link to="/results">Résultats</Link>
+        )}
+      </LinkWrapper>
+    </SurveyContainer>
   )
 }
+
+export default Survey
