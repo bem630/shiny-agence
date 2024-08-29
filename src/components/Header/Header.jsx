a63ec87e0229b345d7fb2086c5f09377f1a2974d
Elhadj Mamadou Bah <bah70495@gmail.com>
2024-08-29 06:03:48 +0100

    ✨ P1C4-begin: Footer > Ajout de layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,14 +5,14 @@ import colors from '../../utils/style/colors';
 import DarkLogo from '../../assets/dark-logo.png'
 
 const StyledLink = styled(Link)`
-    padding: 15px;
-    color: #8186a0;
-    text-decoration: none;
-    font-size: 18px;
-    ${(props) =>
-        props.$isFullLink &&
-        `color: white; border-radius: 30px; background-color: ${colors.primary};`
-    }
+  padding: 15px;
+  color: #8186a0;
+  text-decoration: none;
+  font-size: 18px;
+  ${(props) =>
+      props.$isFullLink &&
+      `color: white; border-radius: 30px; background-color: ${colors.primary};`
+  }
 `
 
 const NavContainer = styled.nav`
