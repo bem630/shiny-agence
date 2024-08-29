d0440da65be3e228b65d54329bc526f2720e91ec
Elhadj Mamadou Bah <bah70495@gmail.com>
2024-08-29 06:20:39 +0100

    ✨ P1C4-begin: Home > Modification du style

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import styled from 'styled-components';
-import HomeIllustration from '../../../assets/home-illustration.svg'
-import colors from '../../../utils/style/colors';
+import styled from 'styled-components'
+import colors from '../../../utils/style/colors'
 import { StyledLink } from '../../../utils/style/Atoms'
+import { useTheme } from '../../../utils/hooks'
+import HomeIllustration from '../../../assets/home-illustration.svg'
 
 const HomeWrapper = styled.div`
   display: flex;
@@ -11,12 +11,14 @@ const HomeWrapper = styled.div`
 
 const HomerContainer = styled.div`
   margin: 30px;
-  background-color: ${colors.background};
+  background-color: ${({ theme }) =>
+    theme === 'light' ? colors.backgroundLight : colors.backgroundDark};
   padding: 60px 90px;
   display: flex;
   flex-direction: row;
   max-width: 1200px;
 `
+
 const LeftCol = styled.div`
   display: flex;
   flex-direction: column;
@@ -31,17 +33,27 @@ const StyledTitle = styled.h2`
   padding-bottom: 30px;
   max-width: 280px;
   line-height: 50px;
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
 `
 
 const Illustration = styled.img`
   flex: 1;
 `
-export default function Home() {
+
+export function sum(a, b) {
+  return a + b
+}
+
+
+function Home() {
+  const { theme } = useTheme()
+
   return (
     <HomeWrapper>
-      <HomerContainer>
+      <HomerContainer theme={theme}>
+      {sum(2, 2)}
         <LeftCol>
-          <StyledTitle>
+          <StyledTitle theme={theme}>
             Repérez vos besoins, on s’occupe du reste, avec les meilleurs
             talents
           </StyledTitle>
@@ -54,3 +66,5 @@ export default function Home() {
     </HomeWrapper>
   )
 }
+
+export default Home
